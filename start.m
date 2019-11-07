02718ab6793ecafb1b0dee43eba81b59e2ec0b0e
Jorge <Jorge@Jorge-SurfaceBook.3521.dhcp.asu.edu>
2019-11-07 16:04:00 -0700

    Updated with a Main GUI program and start file

---
+++ start.m
@@ -0,0 +1,20 @@
+%% MAE 547 Group Project
+% -----------------------------------------------------------------------
+% Created by: (in aphabetical order by last name)
+% Ibrahim Hasan
+% Xiaotian Shen
+% Jorge Soldevila
+% Michael Tucker
+% Nan Xu
+
+% This project is to demonstrate the parameters obtained by the
+% manipulating various joints, as well as assigning various link types.
+% This project is meant to be a learning tool for anyone to use.
+
+% This program will require the use of the robotics toolbox provided by 
+
+%clearing all variables
+clear; clc; close all;
+
+%LAUNCHES THE MAINGUI APP
+MainGUI
